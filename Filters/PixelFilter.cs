67fcff8b23b95f10b828e440fe1fa9300f6e84da
Владимир <fwthedream@mail.ru>
2019-01-28 03:10:08 +0700

    Added first delegates.

--- Filters/PixelFilter.cs
+++ Filters/PixelFilter.cs
@@ -1,8 +1,24 @@
-﻿namespace MyPhotoshop
+﻿using System;
+
+namespace MyPhotoshop
 {
-    public abstract class PixelFilter<TParameters> : ParametrizedFilter<TParameters>
+    public class PixelFilter<TParameters> : ParametrizedFilter<TParameters>
         where TParameters : IParameters, new()
     {
+        private readonly string filterName;
+        private Func<Pixel, TParameters, Pixel> processor;
+
+        public PixelFilter(string name, Func<Pixel, TParameters, Pixel> process)
+        {
+            filterName = name;
+            processor = process;
+        }
+
+        public override string ToString()
+        {
+            return filterName;
+        }
+
         public override Photo Process(Photo original, TParameters parameters)
         {
             var result = new Photo(original.width, original.height);
@@ -10,14 +26,10 @@
             {
                 for (int y = 0; y < result.height; y++)
                 {
-                    result[x, y] = ProcessPixel(original[x, y], parameters);
+                    result[x, y] = processor(original[x, y], parameters);
                 }
             }
-
             return result;
         }
-
-        public abstract Pixel ProcessPixel(Pixel original, TParameters parameters);
-
     }
 }
--- Main.cs
+++ Main.cs
@@ -3,15 +3,27 @@ using System.Windows.Forms;
 
 namespace MyPhotoshop
 {
-	class MainClass
-	{
+    class MainClass
+    {
         [STAThread]
-		public static void Main (string[] args)
-		{
-			var window=new MainWindow();
-            window.AddFilter(new GrayScaleFilter());
-            window.AddFilter (new LighteningFilter());
-			Application.Run (window);
-		}
-	}
+        public static void Main(string[] args)
+        {
+            var window = new MainWindow();
+            window.AddFilter(new PixelFilter<EmptyParameters>(
+                "Оттенки серого",
+                (original, param) =>
+                {
+                    var lightness = original.R + original.G + original.B;
+                    lightness /= 3;
+                    return new Pixel(lightness, lightness, lightness);
+                }
+                ));
+            window.AddFilter(new PixelFilter<LighteningParameters>(
+                "Осветление / затемнение",
+                (original, parameters)
+                => original * parameters.Coefficient
+                ));
+            Application.Run(window);
+        }
+    }
 }
